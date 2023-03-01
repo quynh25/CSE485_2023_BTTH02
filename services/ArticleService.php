60716efc9aaa9b86598eaffda5b70f89f148772c
quynh25 <ttquynh25052002@gmail.com>
2023-03-01 20:18:47 +0700

    css/images

--- services/ArticleService.php
+++ services/ArticleService.php
@@ -4,21 +4,21 @@ include("models/Article.php");
 class ArticleService{
     public function getAllArticles(){
         // 4 bước thực hiện
-       $dbConn = new DBConnection();
-       $conn = $dbConn->getConnection();
+    //    $dbConn = new DBConnection();
+    //    $conn = $dbConn->getConnection();
 
-        // B2. Truy vấn
-        $sql = "SELECT * FROM article INNER JOIN category ON article.category_id=category.id";
-        $stmt = $conn->query($sql);
+    //     // B2. Truy vấn
+    //     $sql = "SELECT * FROM article INNER JOIN category ON article.category_id=category.id";
+    //     $stmt = $conn->query($sql);
 
-        // B3. Xử lý kết quả
-        $articles = [];
-        while($row = $stmt->fetch()){
-            $article = new Article($row['title'], $row['summary'], $row['name']);
-            array_push($articles,$article);
-        }
-        // Mảng (danh sách) các đối tượng Article Model
+    //     // B3. Xử lý kết quả
+    //     $articles = [];
+    //     while($row = $stmt->fetch()){
+    //         $article = new Article($row['title'], $row['summary'], $row['name']);
+    //         array_push($articles,$article);
+    //     }
+    //     // Mảng (danh sách) các đối tượng Article Model
 
-        return $articles;
+    //     return $articles;
     }
 }
